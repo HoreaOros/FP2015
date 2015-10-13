88099d5fa57512016ef6d07006876e027b0c858f
Horea Oros <horea.oros@gmail.com>
2015-10-13 11:59:34 +0300

    first commit

---
+++ Basics/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Teste de paritate");
+            P1();
+
+            Console.WriteLine("2. Suma primelor n numere naturale nenule");
+            P2();
+
+            Console.WriteLine("3. Algoritmul lui Euclid");
+            Euclid();
+        }
+
+        private static void Euclid()
+        {
+            int a, b, r, ca, cb; ;
+
+            a = ExtractInt();
+            b = ExtractInt();
+
+            ca = a;
+            cb = b;
+            while (b != 0)
+            {
+                r = a % b;
+                a = b;
+                b = r;
+            }
+            Console.WriteLine("Cel mai mare divizor comun al numerelor {0} si {1} este {2}", ca, cb, a);
+        }
+
+        private static void P2()
+        {
+            int n = 0;
+            n = ExtractInt();
+
+            Console.WriteLine("Suma numerelor de la 1 la {0} este {1}", n, n * (n + 1) / 2);
+        }
+
+        private static int ExtractInt()
+        {
+            Console.WriteLine("Introduceti un numar natural: ");
+
+            string line;
+            line = Console.ReadLine();
+
+            int n = 0;
+
+            try
+            {
+                n = Int32.Parse(line);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Oops! Something went wrong. This is really embarassing! We'll do our best to address this issue in a future version of our code.");
+            }
+
+            return n;
+        }
+
+        private static void P1()
+        {
+            int n = 0;
+
+            n = ExtractInt();
+
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("Numarul introdus este par.");
+            }
+            else
+            {
+                Console.WriteLine("Numarul introdus este impar.");
+            }
+
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("What is the answer to everything?");
+
+
+            string line;
+
+            line = Console.ReadLine();
+
+
+        }
+    }
+}
