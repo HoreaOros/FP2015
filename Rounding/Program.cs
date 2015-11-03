74e96b8b5c6b0a4c44e1bb8a2f4996133bd55415
Horea Oros <horea.oros@gmail.com>
2015-11-03 11:47:34 +0200

    Structuri - Enumerari

---
+++ Rounding/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rounding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double suma = 0.0;
+            int n = 1000000;
+
+            for (int i = 0; i < n; i++)
+            {
+                suma = suma + 0.01;
+            }
+            Console.WriteLine("Suma este: {0}", suma);
+
+            double na = 1.0 / 0;
+
+            Console.WriteLine(na);
+        }
+    }
+}
