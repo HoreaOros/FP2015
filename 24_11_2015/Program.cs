3d398979c76baca9b8eeca6b79c5e765c3c3af7a
Horea Oros <horea.oros@gmail.com>
2015-11-24 10:46:21 +0200

    Curs 24 Nov. 2015

---
+++ 24_11_2015/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_11_2015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v;
+
+            int n;
+
+            n = Util.ExtractInt("Introduceti dimensiunea vectorului");
+            
+        }
+    }
+}
