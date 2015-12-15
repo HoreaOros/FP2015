7161425d1700c9c484444eb03d7c41ec8757cea4
Horea Oros <horea.oros@gmail.com>
2015-12-15 18:00:58 +0200

    add 15.12.2015

--- 24_11_2015/Program.cs
+++ 24_11_2015/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _24_11_2015
 {
@@ -15,7 +12,35 @@ namespace _24_11_2015
             int n;
 
             n = Util.ExtractInt("Introduceti dimensiunea vectorului");
-            
+
+            v = new int[n]; // toate elementele se intializeaza la zero
+
+            // initializare explicita
+            Random rnd = new Random();
+            for (int i = 0; i < v.Length; i++)
+            {
+                v[i] = rnd.Next(100);
+            }
+
+            PrintArray(v, "Elementele generate:");
+
+            //Array.Sort(v);
+
+            //Util.SelectionSort(v);
+
+            Util.InsertionSort(v);
+            PrintArray(v, "Elementele dupa sortare:");
+
+
+        }
+
+        private static void PrintArray(int[] v, string s)
+        {
+            Console.WriteLine(s);
+            foreach (var item in v)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
